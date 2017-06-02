92ab3006c9e0e3c6c28ac3a721b0d517484c4d2a
Mihaela Olteanu <mihaela.olteanu@siemens.com>
2017-06-02 11:52:06 +0200

    no message

---
+++ kdev-bank/src/test/java/com/kdev/bank/ListAccountsTest.java
@@ -0,0 +1,12 @@
+package com.kdev.bank;
+
+import org.junit.Test;
+
+public class ListAccountsTest {
+
+    @Test
+    public void initially_there_are_no_accounts() {
+	Bank bank = new Bank();
+
+    }
+}
